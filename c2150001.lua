1525358ec7acf304822212b0e3773e87ec5afd2f
ZisIsNotZis <ZisIsNotZis@Gmail.com>
2018-12-18 16:33:01 +0800

    Update c2150001.lua

--- c2150001.lua
+++ c2150001.lua
@@ -5,47 +5,86 @@ function c2150001.initial_effect(c)
 	e:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e:SetCode(EVENT_ADJUST)
 	e:SetRange(LOCATION_MZONE)
-	e:SetOperation(c2150001.xyzo)
+	e:SetOperation(c2150001.oXyz)
 	c:RegisterEffect(e)
 	e=Effect.CreateEffect(c)
 	e:SetType(EFFECT_TYPE_FIELD)
 	e:SetCode(EFFECT_DISABLE_FIELD)
 	e:SetRange(LOCATION_MZONE)
 	e:SetProperty(EFFECT_FLAG_REPEAT)
-	e:SetOperation(c2150001.diso)
+	e:SetOperation(c2150001.oDs)
 	c:RegisterEffect(e)
 	e=Effect.CreateEffect(c)
 	e:SetType(EFFECT_TYPE_FIELD)
 	e:SetCode(EFFECT_DISABLE)
 	e:SetRange(LOCATION_MZONE)
 	e:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e:SetOperation(c2150001.nego)
+	e:SetTarget(c2150001.oNg)
+	e:SetValue(1)
+	c:RegisterEffect(e)
+	e=e:Clone()
+	e:SetCost(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	c:RegisterEffect(e)
+	e=Effect.CreateEffect(c)
+	e:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e:SetCode(EVENT_LEAVE_FIELD)
+	e:SetRange(255)
+	e:SetOperation(c2150001.oLeave)
 	c:RegisterEffect(e)
 end
-function c2150001.xyzo(e,tp)
-	local g=Duel.GetOverlayGroup(0,LOCATION_MZONE,LOCATION_MZONE)
-	Duel.SendtoGrave(g,REASON_EFFECT)
-	Duel.Damage(1-tp,g:FilterCount(c2150000.xyzf,nil,1-tp)*800,REASON_EFFECT)
+function c2150001.oXyz(e,tp)
+	Duel.SendtoGrave(Duel.GetOverlayGroup(0,LOCATION_MZONE,LOCATION_MZONE),REASON_EFFECT)
 end
-function c2150001.xyzf(c,tp)
-	return c:IsControler(tp) and c:IsLocation(LOCATION_GRAVE)
-end
-function c2150001.diso(e,tp)
+function c2150001.oDs(e,tp)
 	local g=Duel.GetMatchingGroup(Card.IsType,0,LOCATION_MZONE,LOCATION_MZONE,nil,TYPE_LINK)
 	local c=g:GetFirst()
 	local z=0
 	while c do
-		z=bit.bor(z,c:GetLinkedZone())
+		local t=c:GetLinkedZone()
+		if c:IsControler(tp) then
+			z=bit.bor(z,t)
+		else
+			z=bit.bor(z,bit.lshift(t,32)+bit.rshift(t,32))
+		end
 		c=g:GetNext()
 	end
 	return z
 end
-function c2150001.nego(e,c):
+function c2150001.oNg(e,t)
 	local g=Duel.GetMatchingGroup(Card.IsType,0,LOCATION_MZONE,LOCATION_MZONE,nil,TYPE_LINK)
-	local i=g:Group.GetFirst()
-	while i do
-		if i:GetLinkedGroup():IsContains(c) then return true end
-		i=g:GetNext()
+	local c=g:GetFirst()
+	while c do
+		if c:GetLinkedGroup():IsContains(t) then return true end
+		c=g:GetNext()
 	end
 	return false
+end
+function c2150001.oLeave(e)
+	local g=Duel.GetMatchingGroup(Card.IsType,0,LOCATION_MZONE,LOCATION_MZONE,nil,TYPE_LINK)
+	local c=g:GetFirst()
+	local h=c:GetLinkedGroup()
+	c=g:GetNext()
+	while c do
+		h:Merge(c:GetLinkedGroup())
+		c=g:GetNext()
+	end
+	if g:GetCount()>0 then
+		c=g:GetFirst()
+		e=Effect.CreateEffect(e:GetHandler())
+		e:SetType(EFFECT_TYPE_SINGLE)
+		e:SetCode(EFFECT_DISABLE)
+		e:SetRange(LOCATION_MZONE)
+		e:SetReset(RESETS_STANDARD+RESET_PHASE+PHASE_END, 2)
+		e:SetValue(1)
+		c:RegisterEffect(e)
+		local f=e:Clone()
+		f:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+		c:RegisterEffect(f)
+		c=g:GetNext()
+		while c do
+			c:RegisterEffect(e)
+			c:RegisterEffect(f)
+			c=g:GetNext()
+		end
+	end
 end
